cab8141aafb432ed88dba2b9389eb90aa763d332
wuxue <wuxue@finchina.com>
2023-06-13 23:14:58 +0800

    feat: 协商缓存和强缓存学习

--- server/http-server.js
+++ server/http-server.js
@@ -11,7 +11,8 @@ const __dirname = dirname(fileURLToPath(shellUrl));
 // 实现一个静态资源服务器
 const server = http.createServer((req,res) => {
   let filepath = resolve(__dirname, join('../public', fileURLToPath(`file:///${req.url}`)));
-  console.log('filepath: ', filepath);
+  const timeStamp = req.headers['if-modified-since'];
+  let httpCode = 200;
   // 存在路径
   if (existsSync(filepath)) {
     const stat = statSync(filepath);
@@ -24,13 +25,24 @@ const server = http.createServer((req,res) => {
     if (!isDir || existsSync(filepath)) {
       // const context = readFileSync(filepath);
       const { ext } = parse(filepath);
-      res.writeHead(200, { 'Content-Type': `${contentType(ext)}` });
-      // TODO 使用流模式读取文件传输文件，避免阻塞
-      const fileStream = createReadStream(filepath)
-      console.log('fileStream: ', fileStream);
-      // return res.end(context);
-      // pipe 方法可以将两个流连接起来，这样数据就会从上游流向下游
-      fileStream.pipe(res);
+      // 协商缓存命中
+      if (timeStamp && Number(timeStamp) === stat.mtimeMs) {
+        httpCode = 304;
+      }
+      res.writeHead(httpCode, { 
+        'Content-Type': `${contentType(ext)}`,
+        'Cache-Control': 'max-age=86400', // 强缓存
+        'Last-Modified': stat.mtimeMs, // 协商缓存
+       });
+       if(httpCode === 200) {
+        // TODO 使用流模式读取文件传输文件，避免阻塞
+        const fileStream = createReadStream(filepath)
+        // return res.end(context);
+        // pipe 方法可以将两个流连接起来，这样数据就会从上游流向下游
+        fileStream.pipe(res);
+      } else { 
+        res.end(); // 如果状态码不是200，不用返回Body
+      }
     }
   } else {
     res.writeHead(404, {'Content-Type': 'text/html'});
